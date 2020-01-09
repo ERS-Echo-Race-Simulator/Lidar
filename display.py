4e938a7fd19f364889f957db3c8d9340876ee722
PietroJomini <pietro.jomini@protonmail.ch>
2020-01-09 14:23:33 +0100

    Fixed radians to cardinal

--- display.py
+++ display.py
@@ -3,25 +3,35 @@ import pygame
 import math
 import time
 
+surface = pygame.display.set_mode((1000, 1000))
+
+def draw(points):
+    surface.fill((0, 0, 0))
+    for (x, y) in points:
+        surface.set_at((int(x) + 500, int(y) + 500), (255, 255, 255))
+    pygame.display.update()
+
 def run():
-    
+
     lidar = RPLidar('/dev/ttyUSB0')
     pygame.init()
-    surface = pygame.display.set_mode((1000, 1000))
-    surface.fill((0, 0, 0))
-    
+
+    points = [ (0, 0) for i in range(361) ]
+    k = 0
+
     try:
         print('Initializing')
         time.sleep(5)
         print('Recording data')
         for new, quality, theta, r in lidar.iter_measurments(max_buf_meas = 800):
-            x = math.cos(theta) * r
-            y = math.sin(theta) * r
-            surface.set_at((int(x + 500), int(y + 500)), (255, 255, 255))
-            if theta > 350 or theta < 15:
-                pass
-                surface.fill((0, 0, 0))
-            pygame.display.update()
+            x = (math.cos(math.radians(theta / math.pi) * math.pi) * r) / 10
+            y = (math.sin(math.radians(theta / math.pi) * math.pi) * r) / 10
+            points[int(theta * 1)] = (x, y)
+            k += 1
+            if k > 100:
+                k = 0
+                draw(points)
+
     except KeyboardInterrupt:
         print('Stopping')
 
